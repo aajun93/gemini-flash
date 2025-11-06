ebcf1a8031747404ceb718e9cb121c5f7de2114c
aajun93 <junaedi.abdullah@hotmail.com>
2025-11-06 14:45:57 +0700

    image-to-text

--- index.js
+++ index.js
@@ -30,5 +30,25 @@ app.post('/generate-text', async (req, res) => {
     }
 });
 
+app.post('/generate-from-image',upload.single('image'), async (req, res)=>{
+    const {prompt} = req.body;
+    const base64Image = req.file.buffer.toString('base64');
+
+    try {
+        const response = await ai.models.generateContent({
+            model:GEMINI_MODEL,
+            contents: [
+                {text: prompt, type: "text"},
+                {inlineData:{data:base64Image, mimeType: req.file.mimetype}}
+            ],
+        });
+
+        res.status(200).json({result:response.text});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({message: e.message})
+    }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>console.log('Server ready  on http://localhost:${PORT}'));
