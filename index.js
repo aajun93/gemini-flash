6066eaf21903786a1fe53bfac9d8b6c67bd14987
Junaedi Abdullah <junaedi.abdullah@hotmail.com>
2025-11-06 12:24:01 +0700

    Implement Gemini AI API endpoints

---
+++ index.js
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs/promises';
+import { GoogleGenAI } from '@google/genai';
+import { connect } from 'http2';
+import { resourceLimits } from 'worker_threads';
+
+const app = express ();
+const upload = multer();
+const ai = new GoogleGenAI({ apiKey: process.env.API_KEY});
+
+const GEMINI_MODEL = "gemini-2.5-flash";
+
+app.use(express.json());
+
+app.post('/generate-text', async (req, res) => {
+    const {prompt} = req.body;
+    
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: prompt
+        });
+
+        res.status(200).json({result: response.text});
+    }   catch (e) {
+        console.log (e);
+        res.status(500).json({message: e.message});
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>console.log('Server ready  on http://localhost:${PORT}'));
